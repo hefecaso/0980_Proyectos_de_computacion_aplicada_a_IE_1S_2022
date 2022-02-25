ee6982124c2b152020637ba1f4b216b792cfc189
hefecaso <hfcarrerasoto@ieee.org>
2022-02-24 23:30:03 -0600

    main

--- Parcial_1/Ocho/ocho.py
+++ Parcial_1/Ocho/ocho.py
@@ -1,26 +1,43 @@
 import random
+import psycopg2
 
-
-
-
-
+connection =  psycopg2.connect(
+    host="localhost",
+    user="postgres",
+    password="123456",
+    database="ocho",
+    port="5432"
+)
 
 while True:
-    dado_1 = random.randint(1, 6)
-    dado_2 = random.randint(1, 6)
+    dado1 = random.randint(1,6)
+    dado2 = random.randint(1,6)
+    print("Dado1: ",dado1)
+    print("Dado2: ", dado2)
 
-    print("Dado 1:", dado_1)
-    print("Dado 2:", dado_2)
-    
     try:
-        if dado_1 + dado_2 == 8:
+        if dado1 + dado2 == 8 :
             print("\nUsted ha ganado! :3")
+            connection.autocommit = True
+            def insertardatos(dados,resultado):
+                cursor = connection.cursor()
+                query = f""" INSERT INTO ocho(dados,resultado) values('{dados}','{resultado}')"""
+                cursor.execute(query)
+                cursor.close()
+            insertardatos(dado1+dado2,"ganó")
             break
-        elif dado_1 + dado_2 == 7:
+
+        elif dado1 + dado2 == 7:
             print("\nUsted ha perdido :(")
+            connection.autocommit = True
+            def insertardatos(dados,resultado):
+                cursor = connection.cursor()
+                query = f""" INSERT INTO ocho(dados,resultado) values('{dados}','{resultado}')"""
+                cursor.execute(query)
+                cursor.close()
+            insertardatos(dado1+dado2,"perdió")
             break
         else:
             print("Sigue lanzando")
-
-    except: #En caso de error:
+    except:
         break
