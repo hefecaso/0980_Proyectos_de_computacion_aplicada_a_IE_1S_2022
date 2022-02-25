61a69791ec98ebdeb7224a1e7c3d07b2d5eab55e
hefecaso <hfcarrerasoto@ieee.org>
2022-02-24 21:45:04 -0600

    main

--- Parcial_1/Ángulos/angulos.py
+++ Parcial_1/Ángulos/angulos.py
@@ -1,9 +1,43 @@
-def CalcularG3(A,B,C):
+import psycopg2
 
-# calcular el angulo en radianes
-G3 = acos(B*B+C*C-A*A)
-# convertir a grados
-G3 = G3*180/pi
+connection =  psycopg2.connect(
+    host="localhost",
+    user="postgres",
+    password="contraseña",
+    database="angulos",
+    port="5432"
+)
 
+while True:
+    try:
+        a1 = int(input("Ingrese el valor del primer angulo: "))
+        a2 = int(input("Ingrese el valor del segundo angulo "))
+        print("Que opción desea realizar: ")
+        op = str(input(""" Menú:
+        1- Encontrar el tercer angulo
+        2- Ver historial \n"""))
+    except: 
+        print ("ERROR")
+        op = '?'
 
-print("El tercer angulo es: {}".format(G3))
+    if op == '1':
+        a3= 180-a1-a2
+        connection.autocommit = True
+        def insertardatos(angulo1,angulo2,angulo3):
+            cursor = connection.cursor()
+            query = f""" INSERT INTO problema2p2 (angulo1,angulo2, angulo3) values('{angulo1}','{angulo2}','{angulo3}')"""
+            cursor.execute(query)
+            cursor.close()
+        insertardatos(a1,a2,a3)
+        print("El angulo 3 es: ",a3)
+        break
+    elif op == '2':
+
+        cursor = connection.cursor()
+        SQL = 'select * from problema2p2;'
+        cursor.execute(SQL)
+        valores = cursor.fetchall()
+        print(valores)
+        break
+    else:
+        print("""has ingresado una opción incorrecta""")
